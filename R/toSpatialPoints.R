2b96f65ee8eefa9d46c8ae79335610fa1615ae9f
eblondel <emmanuel.blondel1@gmail.com>
2015-10-16 12:57:40 +0200

    provide 'toSpatialPoints' utility

---
+++ R/toSpatialPoints.R
@@ -0,0 +1,35 @@
+# toSpatialPoints.R
+# Authors:
+#  Emmanuel Blondel <emmanuel.blondel@fao.org>
+#
+# Creation Date: 2015/10/16
+# Revision Date: -
+#=======================
+
+# Convert a data.frame with lonlat columns to a SpatialPoints object
+#
+# Arguments:
+# - 'x' an object of class "data.frame"
+# - 'lonlat' a vector of length 2 given the lon/lat column names e.g. c("Lon","Lat")
+# - 'verbose' TRUE (by default) to display logs
+#
+toSpatialPoints <- function(x, lonlat, verbose = TRUE){
+  if(missing(lonlat) | is.null(lonlat)) stop("Lon/Lat columns are missing")
+  if(length(lonlat) != 2) stop("lonlat should be a vector of length 2")
+  if(class(lonlat) != "character") stop("lonlat should be of class 'character'")
+  
+  #geo-reference the data
+  if(verbose) message(paste0("Input data has ", nrow(x), " records"))
+  qFilter <- is.na(x[,lonlat[1]]) | x[,lonlat[1]]=="" | is.na(x[,lonlat[2]]) | x[,lonlat[2]]==""
+  toremove <- x[qFilter,]
+  if(nrow(toremove) > 0){
+    if(verbose) message(paste0("Removing ", nrow(toremove), " records with empty coordinates"))
+    x <- x[!qFilter,]
+  }
+  x[,lonlat[1]] <- as.numeric(x[,lonlat[1]])
+  x[,lonlat[2]] <- as.numeric(x[,lonlat[2]])
+  spdf <- x
+  coordinates(spdf) <- lonlat
+  proj4string(spdf) <- CRS("+init=epsg:4326")
+  return(spdf)
+}
