8f5dc07923b3724ea56c078bce0d5d6dbe1ae31e
eblondel <emmanuel.blondel1@gmail.com>
2018-08-15 15:55:23 +0200

    update date / Rdocs

--- R/RFigisGeo.R
+++ R/RFigisGeo.R
@@ -11,7 +11,7 @@
 #'    Package: \tab RFigisGeo\cr
 #'    Type: \tab Package\cr
 #'    Version: \tab 0.3\cr
-#'    Date: \tab 2018-02-14\cr
+#'    Date: \tab 2018-08-15\cr
 #'    License: \tab GPL(>=2.0)\cr
 #'    LazyLoad: \tab yes\cr
 #'  }
