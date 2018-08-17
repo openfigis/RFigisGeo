04dc712e3b3fc72a902f0c4e15e626c7e0f5bdfa
Emmanuel Blondel <emmanuel.blondel1@gmail.com>
2018-08-17 15:24:26 +0200

    Update clipTo.R

--- R/clipTo.R
+++ R/clipTo.R
@@ -10,7 +10,18 @@
 #' @author Emmanuel Blondel \email{emmanuel.blondel1@@gmail.com}
 #' 
 clipToExtent <- function(sp, bb){
-  if(class(bb) == "matrix") b_poly <- as(extent(as.vector(t(bb))), "SpatialPolygons")
+  
+  if(!is(bb, "matrix") stop("'bb' argument should be a 'matrix' object")
+  
+  #first check bb
+  sp.bbox <- bbox(sp)
+  if(bb[1,1] < sp.bbox[1,1]) bb[1,1] <- sp.bbox[1,1]
+  if(bb[2,1] < sp.bbox[2,1]) bb[2,1] <- sp.bbox[2,1]
+  if(bb[1,2] > sp.bbox[1,2]) bb[1,2] <- sp.bbox[1,2]
+  if(bb[2,2] > sp.bbox[2,2]) bb[2,2] <- sp.bbox[2,2]
+     
+  #coonvert bb to poly
+  b_poly <- as(extent(as.vector(t(bb))), "SpatialPolygons")
   else b_poly <- as(extent(bb), "SpatialPolygons")
   proj4string(b_poly) <- CRS(proj4string(sp))
   return(RFigisGeo::intersection(sp, b_poly))
